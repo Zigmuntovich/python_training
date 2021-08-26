1565215eb2c3a67af8c95da1b3c2fc26e2062d49
Serhiy Kononenko <serhiy.kononenko@emotorwerks.com>
2021-08-26 21:21:18 +0300

    a dedicated helper methods and classes

---
+++ data_class.py
@@ -0,0 +1,10 @@
+class Data:
+    def __init__(self, p1, p2, t1, t2, heat_power, static_height, t11, t21=70):
+        self.p1: int = p1
+        self.p2: int = p2
+        self.t1: int = t1
+        self.t2: int = t2
+        self.heat_power: int = heat_power
+        self.static_height: int = static_height
+        self.t11: int = t11
+        self.t21: int = t21
